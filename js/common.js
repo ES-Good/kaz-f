de20011ec1ffb8902ebdb7c3e5564e9229c60366
Максим Казачков <evgenshtanko@ya.ru>
2020-08-27 18:08:01 +0400

    'test'

--- js/common.js
+++ js/common.js
@@ -3,23 +3,49 @@ const form = document.querySelector('.form');
 const closeForm = document.querySelector('.close-form');
 let titleForm = document.querySelector('.title-form')
 
-for (let i = 0; i < card.length; i++) {
-  card[i].onclick = function (){
-    showForm(this.firstElementChild.innerText)
+if (window.innerWidth <= 576) {
+  for (let i = 0; i < card.length; i++) {
+    card[i].onclick = function (){
+      showMobileForm(i, this.firstElementChild.innerText);
+    }
+  }
+}else if(window.innerWidth > 576){
+  for (let i = 0; i < card.length; i++) {
+    card[i].onclick = function (){
+      showForm(this.firstElementChild.innerText);
+    }
+  }
+}
+
+function showMobileForm(num, title){
+  closeForm.classList.add('close-form-show');
+  titleForm.innerText = title + ' ' + '?';
+  if(num >= 0 && num < 3){
+    setTimeout(function(){
+      form.style.top = '23%';
+    }, 50);
+  }else if(num >= 3 && num <= 6){
+    setTimeout(function(){
+      form.style.top = '33%';
+    }, 50);
+  }else if(num > 6){
+    setTimeout(function(){
+      form.style.top = '50%';
+    }, 50);
   }
 }
 
 function showForm(title) {
-  titleForm.innerText = title;
+  titleForm.innerText = title + ' ' + '?';
   closeForm.classList.add('close-form-show');
   setTimeout(function () {
-    form.classList.add('form-show');
+    form.style.top = '50%';
   },50);
 }
 
 document.onclick = function (event) {
   if (event.target.classList.contains('close-form')) {
-    form.classList.remove('form-show');
+    form.style.top = '-50%';
     setTimeout(function () {
       closeForm.classList.remove('close-form-show');
     }, 500)

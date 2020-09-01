c710258f1561f54874dcbf9ad2c2f524134fce9a
Максим Казачков <evgenshtanko@ya.ru>
2020-09-01 14:23:32 +0400

    'form'

--- js/common.js
+++ js/common.js
@@ -19,18 +19,18 @@ if (window.innerWidth <= 450) {
 
 function showMobileForm(num, title){
   closeForm.classList.add('close-form-show');
-  titleForm.innerText = title + ' ' + '?';
+  titleForm.innerText = title;
   if(num >= 0 && num < 3){
     setTimeout(function(){
       form.style.top = '23%';
     }, 50);
-  }else if(num >= 3 && num <= 6){
+  }else if(num >= 3 && num <= 4){
     setTimeout(function(){
       form.style.top = '33%';
     }, 50);
-  }else if(num > 6){
+  }else if(num >= 5){
     setTimeout(function(){
-      form.style.top = '50%';
+      form.style.top = '45%';
     }, 50);
   }
 }
@@ -44,7 +44,7 @@ function showForm(title) {
 }
 
 document.onclick = function (event) {
-  if (event.target.classList.contains('close-form') || event.target.classList.contains('btn-close-form')) {
+  if (event.target.classList.contains('btn-close-form')) {
     form.style.top = '-50%';
     setTimeout(function () {
       closeForm.classList.remove('close-form-show');

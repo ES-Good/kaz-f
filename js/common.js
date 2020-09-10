80c797ea3994c999a158f4f0557a524fce91d32e
Максим Казачков <evgenshtanko@ya.ru>
2020-09-10 14:33:31 +0400

    'fixed_form'

--- js/common.js
+++ js/common.js
@@ -8,37 +8,42 @@ let inpTel = document.getElementById('tel');
 let counter = 1;
 
 
-if (window.innerWidth <= 576) {
-  for (let i = 0; i < card.length; i++) {
-    card[i].onclick = function (){
-      showMobileForm(i, this.firstElementChild.innerText);
-    }
-  }
-}else if(window.innerWidth > 576){
-  for (let i = 0; i < card.length; i++) {
-    card[i].onclick = function (){
-      showForm(this.firstElementChild.innerText);
-    }
+// if (window.innerWidth <= 576) {
+//   for (let i = 0; i < card.length; i++) {
+//     card[i].onclick = function (){
+//       showMobileForm(i, this.firstElementChild.innerText);
+//     }
+//   }
+// }else if(window.innerWidth > 576){
+//   for (let i = 0; i < card.length; i++) {
+//     card[i].onclick = function (){
+//       showForm(this.firstElementChild.innerText);
+//     }
+//   }
+// }
+for (let i = 0; i < card.length; i++) {
+  card[i].onclick = function (){
+    showForm(this.firstElementChild.innerText);
   }
 }
 
-function showMobileForm(num, title){
-  closeForm.classList.add('close-form-show');
-  titleForm.innerText = title;
-  if(num >= 0 && num < 3){
-    setTimeout(function(){
-      form.style.top = '23%';
-    }, 50);
-  }else if(num >= 3 && num <= 5){
-    setTimeout(function(){
-      form.style.top = '33%';
-    }, 50);
-  }else if(num >= 6){
-    setTimeout(function(){
-      form.style.top = '50%';
-    }, 50);
-  }
-}
+// function showMobileForm(num, title){
+//   closeForm.classList.add('close-form-show');
+//   titleForm.innerText = title;
+//   if(num >= 0 && num < 3){
+//     setTimeout(function(){
+//       form.style.top = '23%';
+//     }, 50);
+//   }else if(num >= 3 && num <= 5){
+//     setTimeout(function(){
+//       form.style.top = '33%';
+//     }, 50);
+//   }else if(num >= 6){
+//     setTimeout(function(){
+//       form.style.top = '50%';
+//     }, 50);
+//   }
+// }
 
 function showForm(title) {
   titleForm.innerText = title;
